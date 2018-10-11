cf2062bc0bfad8745d85392181e5a69492d09764
Hong Phuc <nguyenhongphuc98@gmail.com>
2018-10-12 00:22:20 +0700

    change remindbirthday activity, using alarmreciver

--- app/src/main/java/com/example/uitstark/dailys_notes/NewBirthdayActivity.java
+++ app/src/main/java/com/example/uitstark/dailys_notes/NewBirthdayActivity.java
@@ -1,13 +1,94 @@
 package com.example.uitstark.dailys_notes;
 
 import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.DatePickerDialog;
+import android.app.PendingIntent;
+import android.app.TimePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
-public class NewBirthdayActivity extends Activity {
+import com.example.uitstark.dailys_notes.ServiceManage.AlarmReceiver;
+
+import java.util.Calendar;
+
+public class NewBirthdayActivity extends Activity implements View.OnClickListener{
+    Button buttonChooseDateBirthday;
+    Button buttonChooseTimeRemindBirthday;
+    Button buttonSaveBirthday;
+    TextView textViewDate;
+    TextView textViewTime;
+
+    private int y,m,d;
+    private int h,mi;
+
+    AlarmManager alarmManager;
+    PendingIntent pendingIntent;
+    final Calendar  calendar=Calendar.getInstance();
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_birthday);
+
+        buttonChooseDateBirthday =findViewById(R.id.btnChooseDateBirthday);
+        buttonChooseTimeRemindBirthday=findViewById(R.id.btnTimeRemindBirthday);
+        buttonSaveBirthday=findViewById(R.id.btnSaveBirthDay);
+
+        textViewDate=findViewById(R.id.tvDateBirthDay);
+        textViewTime=findViewById(R.id.tvTimeBirthDay);
+
+        buttonChooseDateBirthday.setOnClickListener(this);
+        buttonChooseTimeRemindBirthday.setOnClickListener(this);
+        buttonSaveBirthday.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v==buttonChooseDateBirthday){
+            d=calendar.get(Calendar.DAY_OF_MONTH);
+            m=calendar.get(Calendar.MONTH);
+            y=calendar.get(Calendar.YEAR);
+
+            DatePickerDialog datePickerDialog=new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                    textViewDate.setText("birthday:"+dayOfMonth+"-"+month+"-"+year);
+                }
+            },y,m,d);
+
+            datePickerDialog.show();
+        }
+        else
+
+            if(v==buttonChooseTimeRemindBirthday){
+
+                h=calendar.get(Calendar.HOUR);
+                mi=calendar.get(Calendar.MINUTE);
+
+                TimePickerDialog timePickerDialog=new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        textViewTime.setText("time remind:"+hourOfDay+"-"+minute);
+                    }
+                },h,mi,false);
+
+                timePickerDialog.show();
+            }
+
+            else { //btn save birthday
+                alarmManager= (AlarmManager) getSystemService(ALARM_SERVICE);
+                Intent intent=new Intent(NewBirthdayActivity.this, AlarmReceiver.class);
+                pendingIntent=PendingIntent.getBroadcast(NewBirthdayActivity.this,0,intent,PendingIntent.FLAG_UPDATE_CURRENT);
+                alarmManager.set(AlarmManager.RTC_WAKEUP,calendar.getTimeInMillis(),pendingIntent);
+            }
     }
 }
--- app/src/main/java/com/example/uitstark/dailys_notes/ServiceManage/AlarmReceiver.java
+++ app/src/main/java/com/example/uitstark/dailys_notes/ServiceManage/AlarmReceiver.java
@@ -3,11 +3,13 @@ package com.example.uitstark.dailys_notes.ServiceManage;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 public class AlarmReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         //c thong tin se duoc tiep nhan o day
         //no co the chay ngam
+        Log.d("TAGGGGGGGGGG:","da nhan duoc thong tin dang ky");
     }
 }
